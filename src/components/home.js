bb30647e791c8b6b26e79d8948c24ffe81997003
maadhavagarwal <157884068+maadhavagarwal@users.noreply.github.com>
2024-06-08 16:17:03 +0530

    second

--- src/components/home.js
+++ src/components/home.js
@@ -7,9 +7,10 @@ function ParentComponent() {
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-05-07&sortBy=publishedAt&apiKey=54aa4d61a86145c5bb234d41395f76d3");
+        const response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-05-08&sortBy=publishedAt&apiKey=54aa4d61a86145c5bb234d41395f76d3");
         const data = await response.json();
         setArticles(data.articles);
+        console.log(articles);
       } catch (error) {
         console.error('Error fetching articles:', error);
       }
--- src/components/screen/HomeScreen.js
+++ src/components/screen/HomeScreen.js
@@ -8,7 +8,7 @@ function HomeScreen({ articles }) {
       <h1 className='text-center m-3'>Latest Tech News</h1>
       <hr />
       <Row>
-        {articles.map((article, index) => (
+        {Array.isArray(articles)&&(articles.map((article, index) => (
           <Col key={index} sm={12} md={6} lg={4} xl={3}>
             <Card style={{maxHeight:"820px"}}>
               <Card.Img src={article.urlToImage}  style={{height:'250px'}} alt="No Image avlable"/>
@@ -20,7 +20,7 @@ function HomeScreen({ articles }) {
               </Card.Body>
             </Card>
           </Col>
-        ))}
+        )))}
       </Row>
     </>
   );
